08295e7730cf52312077cf360b1ca9fb47188633
sysfab <sysfab64@gmail.com>
2025-10-05 18:52:46 +0000

    update: rebuild

--- users/sysfab/hyprland/main.nix
+++ users/sysfab/hyprland/main.nix
@@ -6,5 +6,7 @@
           "$mod, Q, exec, kitty"
           "$mod, R, exec, pkill wofi || wofi --show drun"
           "$mod, C, killactive"
+          "$mod, mouse:272, movewindow"
+          "$mod, mouse:273, resizewindow"
      ];
 }
