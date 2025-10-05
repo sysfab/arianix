2fac3e32ea604548317c1dea8d6c42cf14869a18
sysfab <sysfab64@gmail.com>
2025-10-05 23:21:27 +0300

    update: rebuild

--- users/sysfab/main.nix
+++ users/sysfab/main.nix
@@ -27,10 +27,11 @@ in
             kitty
             nautilus
 
+            font-awesome
+
             noto-fonts
             noto-fonts-cjk-sans
             noto-fonts-cjk-serif
-
             noto-fonts-emoji
 
             google-chrome
