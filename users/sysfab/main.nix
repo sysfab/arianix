47fe1275a5261d0ea8add0099adc9cde4bc90dd5
sysfab <sysfab64@gmail.com>
2025-10-05 20:10:26 +0000

    update: rebuild

--- users/sysfab/main.nix
+++ users/sysfab/main.nix
@@ -45,7 +45,7 @@ in
             settings = import ./hyprland/main.nix {};
         };
 
-        home.file."./.config/waybar/config".source = waybar_css;
+        home.file.".config/waybar/style.css".source = waybar_css;
 
         xdg.portal = {
             enable = true;
