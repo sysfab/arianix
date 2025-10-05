15d484afafc5b9f638e444871b1bcbec535d377d
sysfab <sysfab64@gmail.com>
2025-10-05 18:54:31 +0000

    update: rebuild

--- users/sysfab/hyprland/main.nix
+++ users/sysfab/hyprland/main.nix
@@ -6,6 +6,9 @@
           "$mod, Q, exec, kitty"
           "$mod, R, exec, pkill wofi || wofi --show drun"
           "$mod, C, killactive"
+     ];
+
+     bindm = [          
           "$mod, mouse:272, movewindow"
      ];
 }
