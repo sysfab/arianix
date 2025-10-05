81b92f673428f505820748eb475481ec8b712402
sysfab <sysfab64@gmail.com>
2025-10-05 19:37:35 +0000

    update: rebuild

--- users/sysfab/hyprland/main.nix
+++ users/sysfab/hyprland/main.nix
@@ -6,9 +6,12 @@ let
     inputs = import ./inputs.nix {};
     monitors = import ./monitors.nix {};
     misc = import ./misc.nix {};
+
+    animations = import ./look/animations.nix {};
 in
 env //
 programs //
 inputs // 
 monitors //
-misc
+misc //
+animations //
