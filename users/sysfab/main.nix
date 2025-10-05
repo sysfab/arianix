47dcc4626b1924318f0be1930e6f7c646bfcb9c1
sysfab <sysfab64@gmail.com>
2025-10-05 18:25:13 +0000

    update: rebuild

--- users/sysfab/main.nix
+++ users/sysfab/main.nix
@@ -38,7 +38,7 @@ in
 
         programs.hyprland = {
             enable = true;
-            package = hyprland.packages."$(nixpkgs.system)".hyprland;
+            package = hyprland.packages.$(nixpkgs.system).hyprland;
         };
 
         wayland.windowManager.hyprland = {
