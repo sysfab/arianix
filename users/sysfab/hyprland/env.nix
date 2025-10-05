57c9c59527042dbf49d68bdf02aa1164ef450f1f
sysfab <sysfab64@gmail.com>
2025-10-05 19:22:18 +0000

    update: rebuild

---
+++ users/sysfab/hyprland/env.nix
@@ -0,0 +1,9 @@
+{}:
+
+{
+    env = [
+        "XCURSOR_SIZE, 24"
+        "HYPRCURSOR_SIZE, 24"
+        "HYPRSHOT_DIR, Pictures/Screenshots/"
+    ]
+}
--- users/sysfab/hyprland/main.nix
+++ users/sysfab/hyprland/main.nix
@@ -1,10 +1,12 @@
 {}:
 
 let
+    env = import ./env.nix {};
     programs = import ./programs.nix {};
     inputs = import ./inputs.nix {};
     monitors = import ./monitors.nix {};
 in
+env //
 programs //
 inputs // 
 monitors
