10621766d7f5fbae1d28229b1d997465801c04c0
sysfab <sysfab64@gmail.com>
2025-10-05 19:11:23 +0300

    init: init flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.aria = nixpkgs.lib.nixosSystem {
+        modules = [
+
+        ];
+    };
+  };
+}
