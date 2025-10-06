8b4dfeb91b39ec73c6ac2fedb5b3ea1f40b1a990
sysfab <sysfab64@gmail.com>
2025-10-06 07:44:14 +0300

    update: rebuild

--- users/sysfab/hyprland/autostart.nix
+++ users/sysfab/hyprland/autostart.nix
@@ -3,8 +3,5 @@
 {
     exec-once = [
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP && systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
-
-        "$notif"
-        "$bar"
     ];
 }
