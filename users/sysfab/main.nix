e26982787ca04dcbd1981fa85b8bd6070ac3da1f
sysfab <sysfab64@gmail.com>
2025-10-06 09:19:51 +0300

    update: rebuild

--- users/sysfab/main.nix
+++ users/sysfab/main.nix
@@ -138,7 +138,9 @@ in
                 enable = true;
                 defaultApplications = {
                     "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
-                    "default-web-browser" = [ "google-chrome.desktop" ]
+                    "x-scheme-handler/http" = [ "google-chrome.desktop" ];
+                    "x-scheme-handler/https" = [ "google-chrome.desktop" ];
+                    "text/html" = [ "google-chrome.desktop" ];
                 };
             };
 
