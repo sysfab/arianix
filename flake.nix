68a04cd4752aa6f7c4c92f42ea65b734ff95bbbd
sysfab <sysfab64@gmail.com>
2025-10-06 08:22:51 +0300

    update: rebuild

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     };
 
 
-    outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
         nixosConfigurations.aria = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
 
