941ab571cc3a37d5af9d25fec5bae429077283ad
sysfab <sysfab64@gmail.com>
2025-10-09 15:24:16 +0300

    update: rebuild

--- users/sysfab/apps/spotify.nix
+++ users/sysfab/apps/spotify.nix
@@ -10,6 +10,7 @@ let
             owner = "Spikerko";
             repo = "spicy-lyrics";
             tag = "5.16.2";
+            hash = "sha256-NOYbHYgvPNi4+H+xorTvJekhSpvNakjnzPcyc8fvtPY=";
         };
     };
 in
