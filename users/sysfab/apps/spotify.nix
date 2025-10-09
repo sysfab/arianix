230c56396a0993fc07abda9aeb75284f2bec6ae8
sysfab <sysfab64@gmail.com>
2025-10-09 15:36:48 +0300

    update: rebuild

--- users/sysfab/apps/spotify.nix
+++ users/sysfab/apps/spotify.nix
@@ -10,17 +10,6 @@ in
         enabledExtensions = with spicePkgs.extensions; [
             adblock
             hidePodcasts
-
-            {
-                src = (pkgs.fetchFromGitHub {
-                    owner = "Spikerko";
-                    repo = "spicy-lyrics";
-                    tag = "5.16.2";
-                    hash = "sha256-NOYbHYgvPNi4+H+xorTvJekhSpvNakjnzPcyc8fvtPY=";
-                }) + /src;
-
-                name = "app.tsx";
-            }
         ];
 
         enabledCustomApps = with spicePkgs.apps; [
