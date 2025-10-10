b6df1bfe25d9a02ffd46247ff5e6995825b09892
SysFAB <sysfab64@gmail.com>
2025-10-10 18:13:55 +0000

    update: rebuild

--- hosts/aria/configuration.nix
+++ hosts/aria/configuration.nix
@@ -7,17 +7,8 @@
         loader = {
             grub = {
                 enable = true;
-                efiSupport = true;        # enables EFI GRUB
-                device = "nodev";         # required for EFI systems
-            
-                extraEntries = ''
-                    menuentry "Windows (Legacy BIOS on /dev/sda2)" {
-                        insmod part_msdos
-                        insmod ntfs
-                        set root='(hd0,msdos2)'
-                        chainloader +1
-                    }
-                '';
+                efiSupport = true;
+                device = "nodev";            
             };
         };
 
@@ -48,12 +39,6 @@
         device = "/dev/disk/by-uuid/d6524d20-622b-417a-97c0-16a9cf867c75";
         fsType = "ext4";
         options = [ "defaults" ];
-    };
-
-    fileSystems."/mnt/windows" = {
-        device = "/dev/disk/by-uuid/D8E60AFDE60ADC1E";
-        fsType = "ntfs";
-        options = [ "rw" "uid=1000" "gid=100" "umask=0022" ];
     };
 
     networking.hostName = "aria";
