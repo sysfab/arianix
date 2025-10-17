54b0ed71255ce1708dbfc2ea03bdb594949bfe3c
SysFAB <sysfab64@gmail.com>
2025-10-17 17:46:26 +0300

    update: rebuild

--- system-packages.nix
+++ system-packages.nix
@@ -5,6 +5,7 @@
     
     programs.nix-ld.enable = {
         enable = true;
+
         libraries = with pkgs; [
             zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
         ];
