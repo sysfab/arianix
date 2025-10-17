a86db83a9a5de60ded7a3cd844a4463cb450880f
SysFAB <sysfab64@gmail.com>
2025-10-17 17:47:23 +0300

    update: rebuild

--- system-packages.nix
+++ system-packages.nix
@@ -9,13 +9,6 @@
         libraries = with pkgs; [
             zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
         ];
-
-        environment.systemPackages = [
-            (pkgs.writeShellScriptBin "python" ''
-                export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
-                exec ${pkgs.python3}/bin/python "$@"
-            '')
-        ];
     };
 
     services.zerotierone = {
