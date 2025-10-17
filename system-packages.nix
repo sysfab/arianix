bd53aa6d44f8913fb94845b9fb94168ce6ceb7ee
SysFAB <sysfab64@gmail.com>
2025-10-17 17:45:45 +0300

    update: rebuild

--- system-packages.nix
+++ system-packages.nix
@@ -3,7 +3,19 @@
 {
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
     
-    programs.nix-ld.enable = true;
+    programs.nix-ld.enable = {
+        enable = true;
+        libraries = with pkgs; [
+            zlib zstd stdenv.cc.cc curl openssl attr libssh bzip2 libxml2 acl libsodium util-linux xz systemd
+        ];
+
+        environment.systemPackages = [
+            (pkgs.writeShellScriptBin "python" ''
+                export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+                exec ${pkgs.python3}/bin/python "$@"
+            '')
+        ];
+    }
 
     services.zerotierone = {
         enable = true;
